766f385f3df8f0829eb49ec9f04470129e97f118
Baajike <baajikeimmanuella@gmail.com>
2024-05-20 00:31:00 +0000

    python self taught files

---
+++ main.py
@@ -0,0 +1,11 @@
+brand = 'Apple'
+
+exchangeRate = 1.2345678
+
+message = 'The price of this %s laptop is %d USD and the exchange rate is %.2f USD to 1 EUR' %(brand, 1299, exchangeRate)
+
+print(message)
+
+print('%5d hi' %(123))
+
+type(brand)
